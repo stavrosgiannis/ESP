ed0ac7440d167a05a370a4a20660916dadc9e450
Stav ros <hackerone1@web.de>
2019-12-01 22:12:28 +0100

    22:12 1/12/2019

--- ESP/ESP.ino
+++ ESP/ESP.ino
@@ -1,26 +1,24 @@
 
-
 /* Define motor controll inputs */
 const int motorRPin1 = 2; // signal pin 1 for the right motor, connect to IN1               
 const int motorRPin2 = 3;  // signal pin 2 for the right motor, connect to IN2
-const int motorRPWM = 5; // enable pin for the right motor (PWM enabled)
+const int motorRPWM = 5; // enable PWM for the right motor
 
 const int motorLPin1 = 4; // signal pin 1 for the left motor, connect to IN3           
 const int motorLPin2 = 7; // signal pin 2 for the left motor, connect to IN4
-const int motorLPWM = 6; // enable pin for the left motor (PWM enabled)
+const int motorLPWM = 6; // enable PWM for the left motor
 
 const int irPins[8] = {A0, A1, A2, A3, A4, A5 , 11 , 12};
-int irSensorDigital[8] = {0,0,0,0,0,0,0,0};
-//int i,j = 0;
-//int d = 1000;
+int irSensorDigital[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
+//Variable werde 2 Bytes jeweils 0 Bits gesetzt
 int irSensors = B00000000; 
 
 int motorLSpeed = 255;
 int motorRSpeed = 255;
 int error = 145;   // 145 best 200  //  normal 255  // mad 0 
+
 void setup() {
-  
   
   Serial.begin(9600);
   
@@ -36,185 +34,135 @@ void setup() {
   /* Set-up IR sensor pins as input */
   for (int i = 0; i <= 7; i++) 
   {pinMode(irPins[i], INPUT);}
- 
 
 }
 
 void loop() {
-     scanD();
-     check(); 
+     scan_data();
+     check_data(); 
 }     
      
-void check( ) 
+void check_data( ) 
 {    
      switch (irSensors) {
-     
-     case B00000000: // on white paper 
-     rightS();
-     break;
-     
-     case B10000000: // leftmost sensor on the line
-     rightS();
-     break;
-      
-     case B01000000:
-     rightS();
-     break;
-     
-     case B00100000: 
-     rightS();
-     break;
-     
-
-     case B00010000: 
-     rightS();
-     break; 
-     
-     case B00001000: 
-     leftS();
-     break;     
-
-     case B00000100: 
-     leftS();
-     break;
-     
-     case B00000010: 
-     leftS();
-     break;
-      
-     case B00000001: 
-     leftS();
-     break;       
-     
-     case B11000000:
-     rightS();
-     break;
-      
-     case B01100000:
-     rightS();
-     break;
-
-     case B00110000:
-     rightS(); 
-     break;
-     
-     case B00011000: 
-     go();
-     break;          
-
-     case B00001100:
-     leftS();
-     break; 
-
-     case B00000110:
-     leftS();
-     break;   
-     
-     case B00000011:
-     leftS();
-     break;          
-       
-     case B11100000:
-     rightS();   
-     break;
-      
-     case B01110000:
-     rightS();
-     break;
-     
-     case B00111000:
-     rightS();
-     break;
-     
-     case B00011100:
-     leftS();
-     break;  
-     
-     case B00001110:
-     leftS();
-     break; 
-    
-     case B00000111:
-     leftS();
-     break;   
-          
-     case B11110000:
-     rightS(); 
-     break; 
-     
-     case B01111000:
-     rightS();       
-     break;
-      
-     case B00111100:
-     go();
-     break;  
-     
-     case B00011110:
-     leftS();
-     break;  
-     
-     case B00001111:
-     leftS();
-     break;  
-          
-     case B11111000:
-     rightS(); 
-     break; 
-     
-     case B01111100:
-     rightS(); 
-     break;
-     
-     case B00111110:
-     leftS();
-     break;
-     
-     case B00011111:
-     leftS();
-     break;
-           
-     case B11111100:
-     rightS(); 
-     break; 
-     
-     case B01111110:
-     go();
-     break;
-     
-     case B00111111:
-     leftS();
-     break;
-     
-    
-     case B11111110:
-     rightS(); 
-     break; 
-     
-     case B01111111:
-     leftS();
-     break;  
-     
-     case B11100111:
-      go();        
-     
-     break;  
-   
- 
-     default:
-     
-     Serial.print("Unhandled case: ");   
-     
+       case B00000000: // on white paper 
+         turn_right();
+         break;
+       case B10000000: // leftmost sensor on the line
+         turn_right();
+         break;
+       case B01000000:
+         turn_right();
+         break;  
+       case B00100000: 
+         turn_right();
+         break;
+       case B00010000: 
+         turn_right();
+         break; 
+       case B00001000: 
+         turn_left();
+         break;     
+       case B00000100: 
+         turn_left();
+         break;
+       case B00000010: 
+         turn_left();
+         break;
+       case B00000001: 
+         turn_left();
+         break;       
+       case B11000000:
+         turn_right();
+         break;
+       case B01100000:
+         turn_right();
+         break;
+       case B00110000:
+         turn_right(); 
+         break;
+       case B00011000: 
+         forward();
+         break;          
+       case B00001100:
+         turn_left();
+         break; 
+       case B00000110:
+         turn_left();
+         break;   
+       case B00000011:
+         turn_left();
+         break;          
+       case B11100000:
+         turn_right();   
+         break;
+       case B01110000:
+         turn_right();
+         break;
+       case B00111000:
+         turn_right();
+         break;
+       case B00011100:
+         turn_left();
+         break;  
+       case B00001110:
+         turn_left();
+         break; 
+       case B00000111:
+         turn_left();
+         break;   
+       case B11110000:
+         turn_right(); 
+         break; 
+       case B01111000:
+         turn_right();       
+         break;
+       case B00111100:
+         forward();
+         break;  
+       case B00011110:
+         turn_left();
+         break;  
+       case B00001111:
+         turn_left();
+         break;  
+       case B11111000:
+         turn_right(); 
+         break; 
+       case B01111100:
+         turn_right(); 
+         break;
+       case B00111110:
+         turn_left();
+         break;
+       case B00011111:
+         turn_left();
+         break;
+       case B11111100:
+         turn_right(); 
+         break; 
+       case B01111110:
+         forward();
+         break;
+       case B00111111:
+         turn_left();
+         break;
+       case B11111110:
+         turn_right(); 
+         break; 
+       case B01111111:
+         turn_left();
+         break;  
+       case B11100111:
+         forward();        
+         break;  
+       default:
+         Serial.println("Unhandled case: ");   
   }
  
 }
 
-
-
-
-
-void rightS() 
-
+void turn_right() 
 {
      Serial.println("                         right motor forward (right spin)");
      analogWrite(motorRPWM, motorRSpeed);
@@ -227,8 +175,7 @@ void rightS()
   
 }
 
-
-void leftS()  //turn left
+void turn_left()  //turn left
 {
      Serial.println("                         left  motor forward (left spin)");
      analogWrite(motorRPWM, motorRSpeed-error);
@@ -241,7 +188,7 @@ void leftS()  //turn left
   
 }
 
-void go()
+void forward()
 {
     Serial.println("                         forward ");
      analogWrite(motorRPWM, motorRSpeed);
@@ -254,7 +201,7 @@ void go()
   
 }
 
-void stopme()
+void stop()
 {
      Serial.println("                         stop");
      analogWrite(motorRPWM, motorRSpeed);
@@ -267,13 +214,10 @@ void stopme()
   
 }
 
-
-
-
-void scanD()
+void scan_data()
 {
   for ( byte count = 0; count < 8;count++ )
-{
-  bitWrite(irSensors, count, !digitalRead( irPins[count] ));
-} 
+    {
+      bitWrite(irSensors, count, !digitalRead( irPins[count] ));
+    } 
 }
